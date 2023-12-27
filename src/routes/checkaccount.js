dcebdeb694a19e8d311475ab1716774090960268
Ari Maulana Firmansyah <65420719+arimaulanafirmansyah@users.noreply.github.com>
2023-12-27 12:39:13 +0700

    Update checkaccount.js

--- src/routes/checkaccount.js
+++ src/routes/checkaccount.js
@@ -1,13 +1,16 @@
 import express from 'express'
-import { checkAccountAOV, checkAccountCOD, checkAccountFF, checkAccountGI, checkAccountML, checkAccountSM } from '../controller/checkaccountController.js'
+import { checkAccountAOV, checkAccountCOD, vidioSubs, vidioTv, vidioPass, checkAccountGI, checkAccountML, checkAccountSM, vidioDaftar, vidioLogin } from '../controller/checkaccountController.js'
 
 const checkaccount = express()
-
-checkaccount.get('/ff/:id', checkAccountFF)
+checkaccount.get('/vidiosubs/:email/:authtokenUser/:tokenUser', vidioSubs)
+checkaccount.get('/vidiotv/:email/:authtokenUser/:tokenUser/:codenya', vidioTv)
+checkaccount.get('/vidiopass/:email/:password/:passwordnew/:authtokenUser/:tokenUser', vidioPass)
 checkaccount.get('/ml/:zoneid/:id', checkAccountML)
 checkaccount.get('/aov/:id', checkAccountAOV)
 checkaccount.get('/cod/:id', checkAccountCOD)
 checkaccount.get('/gi/:id', checkAccountGI)
 checkaccount.get('/sm/:id', checkAccountSM)
+checkaccount.get('/vidiologin/:email/:password', vidioLogin)
+checkaccount.get('/vidiodaftar/:email/:password', vidioDaftar)
 
 export default checkaccount
